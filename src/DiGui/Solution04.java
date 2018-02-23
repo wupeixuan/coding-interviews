d33e5984a657c678410d70ce0837d3db949cadcf
wupeixuan <18603203910@163.com>
2018-02-23 20:42:52 +0800

    CodingInterviews

---
+++ src/DiGui/Solution04.java
@@ -0,0 +1,14 @@
+package DiGui;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
+public class Solution04 {
+    public static void main(String[] args) {
+        JumpFloor(10);
+    }
+
+    public static int JumpFloor(int target) {
+        return target;
+    }
+}
