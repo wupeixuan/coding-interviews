1865af2f4af8050c5b2a8370e7294a61532e13ca
wupeixuan <18603203910@163.com>
2018-03-27 22:31:16 +0800

    跳台阶

--- src/Recursion/Solution04.java
+++ src/Recursion/Solution04.java
@@ -1,18 +1,37 @@
 package Recursion;
 
 /**
+ * 跳台阶
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  */
 public class Solution04 {
     public static void main(String[] args) {
         Solution04 solution04 = new Solution04();
-        System.out.println(solution04.JumpFloor_2(3));
+        System.out.println(solution04.JumpFloor_3(3));
 
     }
 
     /**
+     * 动态规划
+     *
+     * @param target
+     * @return
+     */
+    public int JumpFloor_3(int target) {
+        if (target == 1) return 1;
+        int[] dp = new int[target];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < target; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[target - 1];
+    }
+
+
+    /**
      * 直接用递归
-     * * 对于第n个台阶来说，只能从n-1或者n-2的台阶跳上来，所以
+     * 对于第n个台阶来说，只能从n-1或者n-2的台阶跳上来，所以
      * F(n) = F(n-1) + F(n-2)
      * f(1)=1
      * f(2)=2
