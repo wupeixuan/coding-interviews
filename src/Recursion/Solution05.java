0632fe5f42ec796a8d8abe71d1594ecd58c5adeb
wupeixuan <18603203910@163.com>
2018-03-27 22:39:26 +0800

    变态跳台阶

--- src/Recursion/Solution05.java
+++ src/Recursion/Solution05.java
@@ -1,13 +1,33 @@
 package Recursion;
 
+import java.util.Arrays;
+
 /**
+ * 变态跳台阶
  * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
  */
 public class Solution05 {
     public static void main(String[] args) {
         Solution05 solution05 = new Solution05();
-        System.out.println(solution05.JumpFloorII_2(3));
+        System.out.println(solution05.JumpFloorII_3(3));
 
+    }
+
+    /**
+     * 动态规划
+     *
+     * @param target
+     * @return
+     */
+    public int JumpFloorII_3(int target) {
+        int[] dp = new int[target];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < target; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j];
+            }
+        }
+        return dp[target - 1];
     }
 
     /**
@@ -39,5 +59,6 @@ public class Solution05 {
         //通过移位计算2的次方
         return 1 << (target - 1);
     }
+
 
 }
