af2c5259bdb84b2cb7cb1f562ac410a17ea7e042
wupeixuan <18603203910@163.com>
2018-04-14 15:52:14 +0800

    两个链表的第一个公共结点

--- src/LinkedList/Solution22.java
+++ src/LinkedList/Solution22.java
@@ -8,6 +8,30 @@ import java.util.HashMap;
  */
 public class Solution22 {
 
+    /**
+     * 设 A 的长度为 a + c，B 的长度为 b + c，其中 c 为尾部公共部分长度，可知 a + c + b = b + c + a。
+     * 当访问 A 链表的指针访问到链表尾部时，令它从链表 B 的头部重新开始访问链表 B；同样地，当访问 B 链表的指针访问到链表尾部时，令它从链表 A 的头部重新开始访问链表 A。这样就能控制访问 A 和 B 两个链表的指针能同时访问到交点。
+     *
+     * @param pHead1
+     * @param pHead2
+     * @return
+     */
+    public ListNode FindFirstCommonNode_3(ListNode pHead1, ListNode pHead2) {
+        ListNode l1 = pHead1, l2 = pHead2;
+        while (l1 != l2) {
+            l1 = (l1 == null) ? pHead2 : l1.next;
+            l2 = (l2 == null) ? pHead1 : l2.next;
+        }
+        return l1;
+    }
+
+    /**
+     * 找出2个链表的长度，然后让长的先走两个链表的长度差，然后再一起走
+     *
+     * @param pHead1
+     * @param pHead2
+     * @return
+     */
     public ListNode FindFirstCommonNode_2(ListNode pHead1, ListNode pHead2) {
         if (pHead1 == null || pHead2 == null)
             return null;
