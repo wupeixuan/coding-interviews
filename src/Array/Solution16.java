e787833f32114cbc5b33ec7407808750784d2d93
wupeixuan <18603203910@163.com>
2018-04-17 13:06:01 +0800

    构建乘积数组

--- src/Array/Solution16.java
+++ src/Array/Solution16.java
@@ -1,7 +1,5 @@
 package Array;
 
-import sun.security.util.Length;
-
 /**
  * 构建乘积数组
  * 给定一个数组A[0,1,...,n-1],请构建一个数组B[0,1,...,n-1],其中B中的元素B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]。
@@ -16,7 +14,6 @@ public class Solution16 {
             System.out.println(B[i]);
         }
     }
-
 
     /**
      * B[i]=A[0]*A[1]*...*A[i-1]*A[i+1]*...*A[n-1]
