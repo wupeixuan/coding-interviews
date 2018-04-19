42c3e5829a84a07197f1e61bb9870b456c2e5112
wupeixuan <18603203910@163.com>
2018-04-19 17:32:40 +0800

    重建二叉树

--- src/Tree/Solution54.java
+++ src/Tree/Solution54.java
@@ -1,14 +1,13 @@
 package Tree;
 
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 重建二叉树
  * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
  */
 public class Solution54 {
-    private Map<Integer, Integer> inOrderNumsIdx = new HashMap<>(); // 缓存中序遍历数组的每个值对应的索引
+    private HashMap<Integer, Integer> inOrderNumsIdx = new HashMap<>(); // 缓存中序遍历数组的每个值对应的索引
 
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         for (int i = 0; i < in.length; i++) {
